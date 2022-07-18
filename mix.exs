4ff1429e1ab792539a5bda12200c0dc3718c3144
cchaiyatad <chaiyatad@gmail.com>
2022-07-18 23:14:00 +0700

    build: update elixir version add dialyzer and credo

--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule MimeSniff.MixProject do
     [
       app: :mime_sniff,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -18,11 +19,18 @@ defmodule MimeSniff.MixProject do
     ]
   end
 
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", [only: [:dev, :test], runtime: false]},
+      {:dialyxir, "~> 1.1", [only: [:dev, :test], runtime: false]}
     ]
   end
 end
