7c691ac07103c8a0173de5a689e2ff36f309a70e
cchaiyatad <chaiyatad@gmail.com>
2022-07-19 15:29:02 +0700

    feat: add binary data byte guard

--- lib/guard.ex
+++ lib/guard.ex
@@ -6,6 +6,35 @@ defmodule MimeSniff.Guard do
 
   @whitespace_byte [<<0x09>>, <<0x0A>>, <<0x0C>>, <<0x0D>>, <<0x20>>]
   @tag_terminating_byte [<<0x20>>, <<0x3E>>]
+  @binary_data_byte [
+    <<0x00>>,
+    <<0x01>>,
+    <<0x02>>,
+    <<0x03>>,
+    <<0x04>>,
+    <<0x05>>,
+    <<0x06>>,
+    <<0x07>>,
+    <<0x08>>,
+    <<0x0B>>,
+    <<0x0E>>,
+    <<0x0F>>,
+    <<0x10>>,
+    <<0x11>>,
+    <<0x12>>,
+    <<0x13>>,
+    <<0x14>>,
+    <<0x15>>,
+    <<0x16>>,
+    <<0x17>>,
+    <<0x18>>,
+    <<0x19>>,
+    <<0x1A>>,
+    <<0x1C>>,
+    <<0x1D>>,
+    <<0x1E>>,
+    <<0x1F>>
+  ]
 
   @doc """
   A whitespace byte (abbreviated 0xWS) is any one of the following bytes:
@@ -18,4 +47,11 @@ defmodule MimeSniff.Guard do
   0x20 (SP), 0x3E (">").
   """
   defguard is_tt(token) when token in @tag_terminating_byte
+
+  @doc """
+  A binary data byte is a byte in the range 0x00 to 0x08 (NUL to BS),
+  the byte 0x0B (VT), a byte in the range 0x0E to 0x1A (SO to SUB),
+  or a byte in the range 0x1C to 0x1F (FS to US).
+  """
+  defguard is_bd(token) when token in @binary_data_byte
 end
--- test/guard_test.exs
+++ test/guard_test.exs
@@ -8,6 +8,9 @@ defmodule MimeSniff.GuardTest do
   defp is_tt?(token) when is_tt(token), do: true
   defp is_tt?(_token), do: false
 
+  defp is_bd?(token) when is_bd(token), do: true
+  defp is_bd?(_token), do: false
+
   describe "is_ws/1" do
     test "return true when input is satisfy" do
       assert is_ws?(<<0x09>>)
@@ -36,6 +39,21 @@ defmodule MimeSniff.GuardTest do
       refute is_tt?(<<0x61>>)
       refute is_tt?(<<224, 184, 129>>)
       refute is_tt?(<<0x20, 0x0A>>)
+    end
+  end
+
+  describe "is_bd/1" do
+    test "return true when input is satisfy" do
+      valid_tokens =
+        Enum.to_list(0x00..0x08) ++ [0x0B] ++ Enum.to_list(0x0E..0x1A) ++ Enum.to_list(0x1C..0x1F)
+
+      assert Enum.all?(valid_tokens, &is_bd?(<<&1>>))
+    end
+
+    test "return false when input is not satisfy" do
+      refute is_bd?(<<0x61>>)
+      refute is_bd?(<<224, 184, 129>>)
+      refute is_bd?(<<0x1A, 0x0A>>)
     end
   end
 end
