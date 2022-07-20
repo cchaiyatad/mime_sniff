4613a9adadb8558a6ab9d4e4d8b6c343b180aa86
cchaiyatad <chaiyatad@gmail.com>
2022-07-20 12:57:34 +0700

    feat: add TextPlainSignature

---
+++ lib/signature/text_plain_signature.ex
@@ -0,0 +1,19 @@
+defmodule MimeSniff.TextPlainSignature do
+  @moduledoc """
+  Functions in this module were implemented
+  as defined in https://mimesniff.spec.whatwg.org/#matching-a-mime-type-pattern
+  """
+  @behaviour MimeSniff.Signature
+  import MimeSniff.Guards
+
+  defstruct []
+
+  @mime_type "text/plain"
+
+  def match(%__MODULE__{}, data) when is_binary(data), do: do_match(data)
+  def match(_, _), do: {:error, :invalid_input_data}
+
+  defp do_match(<<>>), do: {:ok, @mime_type}
+  defp do_match(<<token::bytes-size(1), rest::binary>>) when not is_bd(token), do: do_match(rest)
+  defp do_match(_), do: {:error, :not_match}
+end
