b72504584b643c4ce79dbddeb7907c998ea1f883
cchaiyatad <chaiyatad@gmail.com>
2022-07-20 09:56:56 +0700

    feat: add MaskedSignature

---
+++ lib/signature/masked_signature.ex
@@ -0,0 +1,65 @@
+defmodule MimeSniff.MaskedSignature do
+  @moduledoc """
+  Functions in this module were implemented
+  as defined in https://mimesniff.spec.whatwg.org/#matching-a-mime-type-pattern
+  """
+  @behaviour MimeSniff.Signature
+  import MimeSniff.Guards
+  use Bitwise
+
+  defstruct byte_pattern: <<>>, pattern_mask: nil, ignored_ws_leading_bytes: false, mime_type: ""
+
+  def match(%__MODULE__{byte_pattern: byte_pattern} = signature, data) when is_binary(data) do
+    with :ok <- valid_signature_pattern(signature),
+         data <- ignored_ws_if_needed(signature, data),
+         true <- String.length(data) >= String.length(byte_pattern) do
+      do_match(signature, data)
+    else
+      {:error, :invalid_pattern} -> {:error, :invalid_pattern}
+      false -> {:error, :invalid_input_data}
+    end
+  end
+
+  def match(_signature, _data), do: {:error, :invalid_data}
+
+  defp valid_signature_pattern(%__MODULE__{byte_pattern: nil}), do: {:error, :invalid_pattern}
+  defp valid_signature_pattern(%__MODULE__{pattern_mask: nil}), do: {:error, :invalid_pattern}
+
+  defp valid_signature_pattern(%__MODULE__{} = signature) do
+    case String.length(signature.byte_pattern) == String.length(signature.pattern_mask) do
+      true -> :ok
+      false -> {:error, :invalid_pattern}
+    end
+  end
+
+  defp ignored_ws_if_needed(
+         %__MODULE__{ignored_ws_leading_bytes: true} = signature,
+         <<token::bytes-size(1), rest::binary>>
+       )
+       when is_ws(token),
+       do: ignored_ws_if_needed(signature, rest)
+
+  defp ignored_ws_if_needed(_signature, data), do: data
+
+  defp do_match(%__MODULE__{} = signature, data),
+    do: do_match(signature, data, signature.byte_pattern, signature.pattern_mask)
+
+  defp do_match(%__MODULE__{mime_type: mime_type}, _, <<>>, <<>>), do: {:ok, mime_type}
+
+  defp do_match(
+         %__MODULE__{} = signature,
+         <<d::bytes-size(1), data_rest::binary>>,
+         <<b::bytes-size(1), byte_pattern_rest::binary>>,
+         <<p::bytes-size(1), pattern_mask_rest::binary>>
+       ) do
+    is_match =
+      (:binary.decode_unsigned(p) &&& :binary.decode_unsigned(d)) == :binary.decode_unsigned(b)
+
+    case is_match do
+      false -> {:error, :not_match}
+      true -> do_match(signature, data_rest, byte_pattern_rest, pattern_mask_rest)
+    end
+  end
+
+  defp do_match(_, _, _, _), do: {:error, :not_match}
+end
---
+++ test/signature/masked_signature_test.exs
@@ -0,0 +1,33 @@
+defmodule MimeSniff.MaskedSignatureTest do
+  use ExUnit.Case
+  alias MimeSniff.MaskedSignature
+
+  @xml_byte_pattern <<0x3C, 0x3F, 0x78, 0x6D, 0x6C>>
+  @xml_pattern_mask <<0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>
+  @xml_signature %MaskedSignature{
+    byte_pattern: @xml_byte_pattern,
+    pattern_mask: @xml_pattern_mask,
+    mime_type: "text/xml",
+    ignored_ws_leading_bytes: true
+  }
+  @utf_16BE_bom_byte_pattern <<0xFE, 0xFF, 0x00, 0x00>>
+  @utf_16BE_bom_pattern_mask <<0xFF, 0xFF, 0x00, 0x00>>
+  @utf_16BE_bom_signature %MaskedSignature{
+    byte_pattern: @utf_16BE_bom_byte_pattern,
+    pattern_mask: @utf_16BE_bom_pattern_mask,
+    mime_type: "text/plain"
+  }
+
+  describe "match/2" do
+    test "return {:ok, text/xml} with valid xml input" do
+      # "    <?xml  "
+      xml_data = <<32, 32, 32, 32, 60, 63, 120, 109, 108, 32, 32>>
+      assert MaskedSignature.match(@xml_signature, xml_data) == {:ok, "text/xml"}
+    end
+
+    test "return {:ok, text/plain} with valid utf_16BE_bom input" do
+      xml_data = <<254, 255, 15, 32>>
+      assert MaskedSignature.match(@utf_16BE_bom_signature, xml_data) == {:ok, "text/plain"}
+    end
+  end
+end
